00660f541da97409b927f9f4e575a3cbde1b50b8
SezSahin <sezg0006@edu.eal.dk>
2018-09-27 05:51:05 +0200

    Worked on _Layout.cshtml, site.css, Post.cs & ForumController.cs.

--- DemensProjekt/Controllers/ForumController.cs
+++ DemensProjekt/Controllers/ForumController.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DemensProjekt.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemensProjekt.Controllers
 {
+    [Route("forum")]
     public class ForumController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
+            var posts = new[]
+            {
+                new Post{}
+            };
+            return View(posts);
+        }
+
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Post post)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            post.Author = User.Identity.Name;
+            post.Posted = DateTime.Now;
             return View();
         }
     }
--- DemensProjekt/Models/Post.cs
+++ DemensProjekt/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,21 @@ namespace DemensProjekt.Models
     {
         public long Id { get; set; }
 
-        
+        [Display(Name = "Titel")]
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(20, MinimumLength = 1,
+            ErrorMessage = "Titlen skal være mellem 1 til 20 karakterer lang")]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [MinLength(1,
+            ErrorMessage = "Dit opslag skal mindst være 1 karakter lang")]
+        public string Body { get; set; }
+
+        public DateTime Posted { get; set; }
     }
 }
