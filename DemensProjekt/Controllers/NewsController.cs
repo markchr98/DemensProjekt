59381494a54e8188b1233bb4c47cc6c77a391be1
Andreas Nelleborg Jakobsen <andr2999@edu.eal.dk>
2018-10-24 10:34:06 +0200

    Working on news

--- DemensProjekt/Controllers/NewsController.cs
+++ DemensProjekt/Controllers/NewsController.cs
@@ -6,11 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DemensProjekt.Controllers
 {
+    [Route("news")]
     public class NewsController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("{year:int}/{month:range(1,12}/{key}")]
+        public IActionResult News(int year, int month, string key)
+        {
+
         }
     }
 }
---
+++ DemensProjekt/Models/News.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemensProjekt.Models
+{
+    public class News
+    {
+        public string newsTitle { get; set; }
+        public string newsText { get; set; }
+        public DateTime newsDate { get; set; }
+    }
+}
